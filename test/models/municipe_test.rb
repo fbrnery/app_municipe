b42b8f8352acde7e1d3859a85b1f531c09a3bd76
Fabricio Miranda <fbrnery@gmail.com>
2022-10-21 18:02:28 -0300

    Teste unitário de model municipe.rb

--- test/models/municipe_test.rb
+++ test/models/municipe_test.rb
@@ -1,6 +1,21 @@
 require "test_helper"
 
 class MunicipeTest < ActiveSupport::TestCase
+
+ test "municipe attributes must not be empty" do
+    municipe = Municipe.new
+    assert municipe.invalid?
+    assert municipe.errors[:nome].any?
+    assert municipe.errors[:cpf].any?
+    assert municipe.errors[:cns].any?
+    assert municipe.errors[:email].any?
+    assert municipe.errors[:nascimento].any?
+    assert municipe.errors[:telefone].any?
+    assert municipe.errors[:image_url].any?
+    assert municipe.errors[:status].any?
+    
+  end
+
   # test "the truth" do
   #   assert true
   # end
